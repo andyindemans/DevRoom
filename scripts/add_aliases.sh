3c14cdf8b788b4036c7743ca2d4591a869a56f0a
Andy Indemans <andy.indemans@outlook.com>
2025-04-18 21:06:40 +0200

    Setup root services

---
+++ scripts/add_aliases.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+declare -A aliases=(
+  ["up"]="docker-compose -f ~/Documents/Repositories/DevRoom/docker-compose.yml up -d"
+  ["down"]="docker-compose -f ~/Documents/Repositories/DevRoom/docker-compose.yml down"
+  ["build"]="docker-compose -f ~/Documents/Repositories/DevRoom/docker-compose.yml build"
+)
+
+for name in "${!aliases[@]}"; do
+  alias_cmd="alias $name='${aliases[$name]}'"
+  
+  if grep -Fxq "$alias_cmd" ~/.bashrc; then
+    echo "Alias '$name' already exists in ~/.bashrc"
+  else
+    echo "$alias_cmd" >> ~/.bashrc
+    echo "Alias '$name' added to ~/.bashrc"
+  fi
+done
+
+source ~/.bashrc
